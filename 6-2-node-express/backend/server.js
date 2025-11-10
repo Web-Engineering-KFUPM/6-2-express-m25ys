17412208bc26f45f0078a54f5a55b33f7e9c9e53
m25ys <myalsuhaibani@gmail.com>
2025-11-10 21:46:08 +0300

    some changes

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,19 +2,20 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-{/*write code for cors*/}
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 
-{/*write code to define routes*/}
 app.get("/", (req, res) => {
   res.send("Welcome to the Quote Generator API");
 });
 
-{/*write code to create server*/}
 app.get("/api/quote", (req, res) => {
   const quote = getRandomQuote();
   res.json({ quote });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
