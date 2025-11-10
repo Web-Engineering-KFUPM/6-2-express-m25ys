482df811f3a528baac34498848e0d73466f1c0cf
m25ys <myalsuhaibani@gmail.com>
2025-11-10 21:34:11 +0300

    serverFile

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,10 +2,19 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 {/*write code for cors*/}
-
+app.use(cors());
 
 {/*write code to define routes*/}
-
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quote Generator API");
+});
 
 {/*write code to create server*/}
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
